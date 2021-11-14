b0923cb5c43ddf1d4bb5fbac8747e24a1c5e6b2b
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2021-11-14 17:09:54 -0500

    added basic route of retrieving unix time

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
-import os
-import base64
-
-from flask import Flask, render_template, request, redirect, url_for, session
-
-from model import Donation
+from flask import Flask
+import time
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return redirect(url_for('all'))
-
-
-@app.route('/donations/<type>')
 def all():
-    donations = Donation.select()
-    return render_template('donations.jinja2', donations=donations)
+    return "The unix time now is {}, (refresh the page for updated time)".format(time.time())
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 6738))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.1.0.0', port=8000)
